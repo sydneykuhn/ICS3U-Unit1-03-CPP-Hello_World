5a12c5b7a0aa8f730e9e6a1ef9a53628bc21aecf
Ubuntu <ubuntu@ip-172-31-90-143.ec2.internal>
2021-09-11 15:29:36 +0000

    added hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Sydney Kuhn All rights reserved
+//
+// Created by: Sydney Kuhn
+// Created on: Sept 2020
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+    int main() {
+    std::cout << "Hello, World!";
+}
